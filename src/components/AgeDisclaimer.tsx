5cd1ed36b3199fdb98138fd29a60f3e735ea5aea
brickwalker <artem.nedostup@gmail.com>
2021-11-23 11:26:41 +0000

    add structure

--- src/components/AgeDisclaimer.tsx
+++ src/components/AgeDisclaimer.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import * as React from "react";
+import { Navigate } from "react-router-dom";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+
+const ageStatus = {
+  title: "Please confirm your age.",
+  description: "You must be 18 or older to use this tool.",
+  pass: "18 or older",
+  fail: "Less than 18",
+};
 
 export default class AgeDisclaimer extends React.Component {
-    render(){
-        return <p>This is Age Disclaimer.</p>;
+
+    constructor(props: any) {
+        super(props);
+        this.checkPass = this.checkPass.bind(this);
     }
+
+  state = {
+    pass: false,
+  };
+
+  checkPass(reply: string) {
+    reply === ageStatus.pass && this.setState({ pass: true });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{ageStatus.description}</div>
+        <DisclaimerDialog checkPass={this.checkPass} />
+        {this.state.pass && <Navigate to="/calculator" />}
+      </div>
+    );
+  }
+}
+
+interface IDDProps {
+    checkPass: (reply: string) => void
+}
+interface IDDState {
+    open: boolean
+}
+
+export class DisclaimerDialog extends React.Component<IDDProps, IDDState> {
+  state = {
+    open: true,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (e: React.BaseSyntheticEvent) => {
+    console.log(e.target.textContent);
+
+    //const message = e.target.textContent;
+
+    this.setState({ open: false });
+    this.props.checkPass(e.target.textContent);
+  };
+
+  render() {
+    return (
+      <Dialog
+        open={this.state.open}
+        onClose={this.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{ageStatus.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {ageStatus.description}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose}>{ageStatus.fail}</Button>
+          <Button onClick={this.handleClose} autoFocus>
+            {ageStatus.pass}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { CssBaseline, Grid } from "@mui/material";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router-dom";
+import { CssBaseline, Grid, AppBar, Toolbar, Typography } from "@mui/material";
+import ShareIcon from "@mui/icons-material/Share";
+import styled from "@emotion/styled";
 
 export default class App extends React.Component {
   render() {
@@ -8,14 +10,45 @@ export default class App extends React.Component {
       <React.Fragment>
         <CssBaseline />
         <Grid container direction="column">
-          <Grid item container>
-            This is my app
+          <Grid item>
+            <TopBar />
           </Grid>
           <Grid item container>
             <Outlet />
           </Grid>
         </Grid>
       </React.Fragment>
+    );
+  }
+}
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+class TopBar extends React.Component {
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <StyledLink to="/">
+            <Typography variant="h6" component="div">
+              underalimit
+            </Typography>
+          </StyledLink>
+          &nbsp; | &nbsp;
+          <StyledLink to="/contacts" style={{ flex: 1 }}>
+            <Typography variant="h6" component="div">
+              Contacts
+            </Typography>
+          </StyledLink>
+          <ShareIcon />
+        </Toolbar>
+      </AppBar>
     );
   }
 }
